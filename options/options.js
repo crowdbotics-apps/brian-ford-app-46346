759cd3f33f07039f4ce77539639d028b6997d596
Crowdbotics <team@crowdbotics.com>
2024-02-14 17:08:38 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "brian-ford-app-46346",
+  url: "https://brian-ford-app-46346.botics.co",
+  api: "https://brian-ford-app-46346.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
